b5c79732394a12deb9e72864cbf5e714657d9a4c
Mohamed Elghool <elghool8759@gmail.com>
2026-04-21 13:01:22 +0200

    fix layout default export issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,23 @@
+import "./globals.css";
+
 export const metadata = {
   title: "دعوة خطوبة خالد ❤️ مريم",
   description: "نتشرف بدعوتكم لحضور حفل خطوبتنا 💍🎉",
-
   openGraph: {
-    title: "Khaled ❤️ Mariam Wedding Invitation",
-    description: "You are invited 💍",
-    images: [
-      {
-        url: "/cover.jpg",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-
-  twitter: {
-    card: "summary_large_image",
+    title: "Khaled ❤️ Mariam Wedding",
+    description: "Wedding Invitation 💍",
     images: ["/cover.jpg"],
   },
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ar">
+      <body>{children}</body>
+    </html>
+  );
+}
