f1efe988b3f0a546ff2506f9489d322c77cea7f9
Mohamed Elghool <elghool8759@gmail.com>
2026-04-21 13:10:18 +0200

    fix layout + og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,24 @@
 import "./globals.css";
+import "./globals.css";
 
 export const metadata = {
   title: "دعوة خطوبة خالد ❤️ مريم",
   description: "نتشرف بدعوتكم لحضور حفل خطوبتنا 💍🎉",
+
   openGraph: {
     title: "Khaled ❤️ Mariam Wedding",
     description: "Wedding Invitation 💍",
+    images: [
+      {
+        url: "/cover.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
     images: ["/cover.jpg"],
   },
 };
