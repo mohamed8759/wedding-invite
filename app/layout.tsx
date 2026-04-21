c0c790b0713d6796fe428039b283863309fd8686
Mohamed Elghool <elghool8759@gmail.com>
2026-04-21 12:47:03 +0200

    fix layout build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,18 @@
+import "./globals.css";
+
 export const metadata = {
   title: "دعوة خطوبة خالد ❤️ مريم",
   description: "نتشرف بدعوتكم لحضور حفل خطوبتنا 💍🎉",
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ar">
+      <body>{children}</body>
+    </html>
+  );
+}
