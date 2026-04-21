d48d031a516b2ab897bc4f0b2c6311d56995b5c9
Mohamed Elghool <elghool8759@gmail.com>
2026-04-21 12:57:24 +0200

    update open graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import "./globals.css";
-
 export const metadata = {
   title: "دعوة خطوبة خالد ❤️ مريم",
   description: "نتشرف بدعوتكم لحضور حفل خطوبتنا 💍🎉",
-};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="ar">
-      <body>{children}</body>
-    </html>
-  );
-}
+  openGraph: {
+    title: "Khaled ❤️ Mariam Wedding Invitation",
+    description: "You are invited 💍",
+    images: [
+      {
+        url: "/cover.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    images: ["/cover.jpg"],
+  },
+};
